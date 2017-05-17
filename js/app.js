2d070b5284c4afba8631bcb449d7e044b3119509
Alex Melagrano <alexmelagrano@gmail.com>
2017-05-17 02:07:45 -0400

    amount raised feature, with css bugs

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/**
+ * Created by alexmelagrano on 5/16/17.
+ */
+
+
+// Simple GET request to see pull the current fund total
+
+// GitHub API endpoint: https://api.github.com/ ...
+
+var main = function(){
+
+    var reqUrl = "https://api.github.com/repos/alexmelagrano/donation-page/contents/amount.txt?ref=master";
+
+    $.ajax({
+        url:reqUrl,
+        dataType: "jsonp",
+        success : function(data)
+        {
+            var amount = atob(data.data.content);
+            console.log("here's the data");
+            console.log(amount);
+            console.log("and here's the specific amount, decoded");
+            console.log(amount);
+            $('#donation-amount').html('$' + amount);
+        },
+        error: function(response){
+            console.log('An error occurred:');
+            console.log(response);
+        }
+    });
+};
+
+$(document).ready(main);
